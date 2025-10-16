78390a3c0ea0b157feabeac72fb219c584ae1929
Steve-Glover <steve.glover@outlook.com>
2025-10-16 18:15:06 -0400

    (add) test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest import TestCase
+from EmotionDetection import emotion_detector
+
+class TestEmotionDetection(TestCase):
+
+    def test_dominant_emotion(self):
+        tests = [
+            ("I am glad this happened", "joy"),
+            ("I am really mad about this", "anger"),
+            ("I feel disgusted just hearing about this", "disgust"),
+            ("I am so sad about this", "sadness"),
+            ("I am really afraid that this will happen", "fear")
+        ]
+        for text, expected in tests:
+            with self.subTest(text=text, expected=expected):
+                result = emotion_detector(text)['dominant_emotion']
+                self.assertEqual(result, expected)
+
+
+if __name__== '__main__':
+    unittest.main(verbosity=2)
