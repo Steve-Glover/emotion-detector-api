86029312b5cdb1c07867dc67e103b35fa7ffba30
Steve-Glover <steve.glover@point32health.org>
2025-10-19 19:47:33 -0400

    (mod) server.py - error handle blank entries.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from EmotionDetection import emotion_detector
 import json
 
@@ -12,6 +12,9 @@ def root():
 @app.route("/emotionDetector")
 def emotionDetector():
     textToAnalyze = request.args.get("textToAnalyze")
+    if textToAnalyze is None or textToAnalyze == "":
+        response =  make_response("empty string supplied", 500) 
+        return response
     detected_emotion = emotion_detector(textToAnalyze)
     return f"For the given statement, the system reponse is {detected_emotion}"
 
