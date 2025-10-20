029db6fd90eebf123d440654ee9b69291533adeb
Steve-Glover <steve.glover@point32health.org>
2025-10-19 20:22:23 -0400

    (mod) server.py - conform with pylint suggestions for best practices

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, request, make_response
+""" Module API Documentation """
+from flask import Flask, render_template, request
 from EmotionDetection import emotion_detector
-import json
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
+    """root route"""
     return render_template("index.html")
 
 @app.route("/emotionDetector")
-def emotionDetector():
-    textToAnalyze = request.args.get("textToAnalyze")
-    detected_emotion = emotion_detector(textToAnalyze)
+def emotion_detector_api():
+    """API emotion detector call"""
+    text_to_analyze = request.args.get("textToAnalyze")
+    detected_emotion = emotion_detector(text_to_analyze)
     if detected_emotion["dominant_emotion"] is None:
         return "Invalid text! Please try again", 200
 
     return f"For the given statement, the system reponse is {detected_emotion}"
 
- 
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
